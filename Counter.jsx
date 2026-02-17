00ed60d9fe90a2a795c5d6575952757926f76961
Satyam6767 <satyamkumar6767@gmail.com>
2026-02-17 14:41:29 +0530

    new change

--- Counter.jsx
+++ Counter.jsx
@@ -11,7 +11,7 @@ function Counter() {
 
     <div>
         <h1>{count}</h1>
-        <button onClick={()=> dispatch(increment())} >add to cart</button>
+        <button onClick={()=> dispatch(increment())} >add to cartttttttt</button>
         <button onClick={()=>dispatch(decrement())}>remove from cart</button>
     </div>
   )
